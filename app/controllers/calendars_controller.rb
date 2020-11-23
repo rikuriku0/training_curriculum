2b621a17c22c1186584c29d78ddc9af13ce8a0a0
rikuriku0 <matsuri726@yahoo.ne.jp>
2020-11-23 11:23:06 +0900

    Revert "Revert "Merge pull request #8 from rikuriku0/second_training""
    This reverts commit a52c291cd921d08a82a18eba5e2bf17c443d2943.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
@@ -34,9 +34,30 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
+
+      
+      wday_num = Date.today.wday+x
+      if wday_num >= 7
+        wday_num = wday_num -7
+      end
+
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wdays => wdays[wday_num]}
+
       @week_days.push(days)
     end
 
   end
+
 end
+
+
+
+
+# ②コントローラーを編集して曜日を取得する記述をする
+#indexのday[:month]　day[:date]のmonthやdateに当たるものをご自身で設定する（キー　バリューのキー）
+# キーが決まったらwdaysという配列とDateオブジェクトを使って具体的に曜日の情報を取得する（カリキュラムのヒントに該当するところ）
+
+# 基礎カリキュラム：キーバリュー
+# 応用：ビューファイルに値を渡す、表示させる
+
+
